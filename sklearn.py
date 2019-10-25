21805d3d6b4784c6e013706572e5a7176774075c
Michael Chau <inventormc@gmail.com>
2019-10-25 06:35:21 -0700

    add func signatures

--- sklearn.py
+++ sklearn.py
@@ -1,4 +1,37 @@
 from sklearn.base import BaseEstimator
-class TuneCV(BaseEstimator)
-            @property
-    def _estimator_type(self):
+class TuneCV(BaseEstimator):
+    def __init__(
+            # see params in doc
+            ):
+        pass
+
+    def _refit(self, X,  y=None, **fit_params):
+        pass
+
+    def fit(self, X, y=None, groups=None, **fit_params):
+        pass
+
+    def score(self, X, y=None):
+        pass
+
+
+class _Trainable(Trainable):
+    def __init__(
+            #see params in doc
+            ):
+        pass
+
+    def _setup(self, config):
+        pass
+
+    def _train(self):
+        pass
+
+    def _save(self, checkpoint_dir):
+        pass
+
+    def _restore(self, checkpoint):
+        pass
+
+
+
